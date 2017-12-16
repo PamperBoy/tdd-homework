0f7bb9494e77f9c4eae452af67e06a7df404773d
Abohte <adinda.bohte@gmail.com>
2017-12-16 13:38:59 +0100

    Pass test for C

--- app/codebreaker.rb
+++ app/codebreaker.rb
@@ -14,19 +14,33 @@ class Codebreaker
     end
 
     def guess(input)
-      correct_input?(input)
+      return if !(correct_input?(input))
       compare_input(input)
     end
 
     def correct_input?(input)
-      return if (input.length == 4 && input.to_i.to_s.length == 4)
+      return true if (input.length == 4 && input.to_i.to_s.length == 4)
       output.puts "Try guessing a number with four digits"
+      return false
     end
 
     def compare_input(input)
       count = 0
       input.each_char { |number| count += 1 if @secret_number.include? number}
-      count == 0 ? (output.puts "") : (output.puts "-")
+      count == 0 ? (output.puts "") : number_correct(input)
     end
+
+    def number_correct(input)
+      count = 0
+      feedback = ""
+      input.each_char do |number|
+        if @secret_number.index(number) == count
+          feedback += "+"
+        end
+        count += 1
+      end
+      feedback.length == 0 ? (output.puts "-") : (output.puts feedback)
+    end
+
   end
 end
