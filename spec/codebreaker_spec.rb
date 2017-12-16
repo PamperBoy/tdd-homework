1654443e8a4688bdf74f27e1e3c53f97158ac5f9
Abohte <adinda.bohte@gmail.com>
2017-12-16 18:58:47 +0100

    separate file for extra test 'guess starts with 0'

--- spec/codebreaker_spec.rb
+++ spec/codebreaker_spec.rb
@@ -227,16 +227,5 @@ RSpec.describe Codebreaker do
       end
     end
 
-    context "guess starts with 0" do
-      let(:secret_number) { '1234' }
-      it "does not ask for new input" do
-        input = '0000'
-        game.start(secret_number)
-        expect(output).to_not receive(:puts).with("Try guessing a number with four digits")
-
-        game.guess(input)
-      end
-
-    end
   end
 end
