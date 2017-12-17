bdd7047574dc25c37193e42075adce1d9cad6718
pamperboy <marc_smalbrugge@live.nl>
2017-12-17 18:00:04 +0100

    Refractor Codebreaker class

--- app/codebreaker.rb
+++ app/codebreaker.rb
@@ -1,6 +1,12 @@
 class Codebreaker
   class Game
     attr_reader :output
+    EXACT_MATCH = "+"
+    NUMBER_MATCH = "-"
+    NO_MATCH = ""
+    WELCOME_MSG = "Welcome to Codebreaker"
+    GUESS_MSG = "Enter guess:"
+    RETRY_MSG = "Try guessing a number with four digits"
 
     def initialize(output)
       @output = output
@@ -8,40 +14,35 @@ class Codebreaker
 
     def start(secret_number)
       @secret_number = secret_number
-
-      output.puts "Welcome to Codebreaker"
-      output.puts "Enter guess:"
+      respond(WELCOME_MSG)
+      respond(GUESS_MSG)
     end
 
-    def match_number_check(input)
-      result = []
-      match_result = []
-      exact_result = []
-
-      @secret_number.each_char.with_index do |snum, i|
-        if input[i] == snum && input.include?(snum)
-          exact_result << "+"
-
-        elsif input.include?(snum)
-          match_result << "-"
-        else
-          result << ""
-        end
-
-      end
-      result = exact_result.concat(match_result)
-      output.puts result.join("")
+    def respond(message)
+      output.puts message
     end
 
     def guess(input)
-      if input.length != 4
-        output.puts "Try guessing a number with four digits"
-      else
-        match_number_check(input)
+      valid_answer?(input) ? respond(RETRY_MSG) : match_number_check(input)
+    end
+
+    private
+
+    def valid_answer?(input)
+      input.length != @secret_number.length
+    end
+
+    def match_number_check(input)
+      match_result, exact_result = [], []
+      @secret_number.each_char.with_index do |seperate_number, i|
+        if input[i] == seperate_number
+          exact_result << EXACT_MATCH
+        elsif input.include?(seperate_number)
+          match_result << NUMBER_MATCH
+        end
       end
-      if input != @secret_number
-        output.puts ""
-      end
+      result = exact_result.concat(match_result).join("")
+      respond(result)
     end
   end
 end
